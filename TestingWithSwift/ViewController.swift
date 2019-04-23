0b7184f34abfc6c13a910c3ade7ef84c5023d859
Victor Capilla <victor.capilla@basetis.com>
2019-04-23 10:49:54 +0200

    First Commit

--- TestingWithSwift/ViewController.swift
+++ TestingWithSwift/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
 
 }
 
