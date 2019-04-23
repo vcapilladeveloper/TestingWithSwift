bfcecf807e4cc832ff784292af791d0e76a69848
Victor Capilla <victor.capilla@basetis.com>
2019-04-23 11:10:59 +0200

    First Commit for Rebase

--- TestingWithSwift/ViewController.swift
+++ TestingWithSwift/ViewController.swift
@@ -21,6 +21,11 @@ class ViewController: UIViewController {
         print("second commit")
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("Testing for rebase")
+    }
+    
 
 }
 
